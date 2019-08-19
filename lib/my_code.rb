eadb8ea007cc77bbb027205512947d77339dbd2a
Ting Ting Ye <tingtingye24@gmail.com>
2019-08-19 18:55:12 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,25 @@
 # Your Code Here
+
+def map(array)
+  newarray = []
+  array.length.times do |index|
+    newarray << yield(array[index])
+  end
+  return newarray
+  
+end
+
+
+def reduce(array, start=nil)
+  if start
+    final = start
+  else 
+  final =array[0]
+  array.shift
+  end
+  array.length.times do |index|
+  final  = yield(final, array[index])
+  end
+  return final
+end
+
